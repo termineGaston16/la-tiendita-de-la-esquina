09d1224664aead1a87bba492b63ddc31910b7e4d
Gaston Termine <gastontermine16@gmail.com>
2024-02-09 00:44:51 -0300

    as

--- js/index.js
+++ js/index.js
@@ -3,12 +3,15 @@
 /* abrir */
 document.querySelector("#btnHeaderIndexNav").addEventListener("click", () => {
     document.querySelector("#idMenuNavIndexFondoNegro").classList.remove("d-none")
-    document.querySelector("#idMenuNavOpen").classList.remove("d-none")
+    document.querySelector("#idMenuNavOpen").classList.remove("opacity-0")
+    document.querySelector("#idMenuNavOpen").classList.add("open")
+
 })
 /* cerrar */
 document.querySelector("#idMenuNavIndexFondoNegro").addEventListener("click", () => {
     document.querySelector("#idMenuNavIndexFondoNegro").classList.add("d-none")
-    document.querySelector("#idMenuNavOpen").classList.add("d-none")
+    document.querySelector("#idMenuNavOpen").classList.add("opacity-0")
+    document.querySelector("#idMenuNavOpen").classList.remove("open")
 })
 
 /* ------------------------- */
@@ -24,31 +27,40 @@ fetch(url)
             let numeroRandom = Math.floor(Math.random() * 25);
 
             let ordenRandom = Math.floor(Math.random() * 3);
-            let ordenSegundo=0;
+            let ordenSegundo = (ordenRandom === 1) ? 2 : 1;
 
-            if (ordenRandom==1) {
-                ordenSegundo=2;
-            }else{
-                ordenSegundo=1;
-            }
-        
             let contendorRow = document.createElement("div");
             contendorRow.classList.add("row");
             contendorRow.innerHTML = `
-                <div class="col-xl-6 col-md-6 col-sm-12"; style="order:${ordenRandom}">
-                    <img src="${todasLasConsolas[numeroRandom].imagenReferente}" alt="" style="width: 50%;">
+                <div class="col-xl-6 col-md-6 col-sm-12 mainIndexPortadasPart1" style="order:${ordenRandom}">
+                    <img src="${todasLasConsolas[numeroRandom].imagenReferente}" alt="" class="mainIndexPortadasPart1Img">
                 </div>
-                <div class="col-xl-6 col-md-6 col-sm-12" style="order:${ordenSegundo}">
-                    <h4>${todasLasConsolas[numeroRandom].compania}</h4>
-                    <p>${todasLasConsolas[numeroRandom].descripcion}</p>
+                <div class="col-xl-6 col-md-6 col-sm-12 mainIndexPortadasPart2" style="order:${ordenSegundo}">
+                    <h4 class="mainIndexPortadasPart2Title">${todasLasConsolas[numeroRandom].compania}</h4>
+                    <p class="mainIndexPortadasPart2Text">${todasLasConsolas[numeroRandom].descripcion}</p>
                 </div>
             `;
+
             document.querySelector("#contendorPortadaIndex").append(contendorRow);
+
+            
+            setTimeout(() => {
+                contendorRow.querySelectorAll('.mainIndexPortadasPart1, .mainIndexPortadasPart2').forEach(element => {
+                    element.style.opacity = 1;
+                });
+            }, 100);
+
+            setTimeout(() => {
+                contendorRow.querySelectorAll('.mainIndexPortadasPart1, .mainIndexPortadasPart2').forEach(element => {
+                    element.style.opacity = 0;
+                });
+            }, 5500);
         }
-        
+
         setInterval(actualizarPortada, 7000);
     })
     .catch(error => console.error('Error al cargar las consolas:', error));
+
 
 
 
